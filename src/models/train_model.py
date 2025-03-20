758c5e687db6a56b1d7d98875f46cc5f9d0d9638
Ubuntu <ubuntu@ip-172-31-37-210.eu-west-1.compute.internal>
2025-03-20 19:07:04 +0000

    LOL

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,44 +1,27 @@
-import sklearn
-import pandas as pd 
-from sklearn import ensemble
-import joblib
+import pandas as pd
 import numpy as np
+from sklearn import ensemble
 import bentoml
-from bentoml.io import NumpyNdarray
 
-#print(joblib.__version__)
-
+# Load data
 X_train = pd.read_csv('data/preprocessed/X_train.csv')
 X_test = pd.read_csv('data/preprocessed/X_test.csv')
-y_train = pd.read_csv('data/preprocessed/y_train.csv')
-y_test = pd.read_csv('data/preprocessed/y_test.csv')
-y_train = np.ravel(y_train)
-y_test = np.ravel(y_test)
+y_train = np.ravel(pd.read_csv('data/preprocessed/y_train.csv'))
+y_test = np.ravel(pd.read_csv('data/preprocessed/y_test.csv'))
 
-rf_classifier = ensemble.RandomForestClassifier(n_jobs = -1)
-
-#--Train the model
+# Train model
+rf_classifier = ensemble.RandomForestClassifier(n_jobs=-1)
 rf_classifier.fit(X_train, y_train)
 
-#--Test the model
-rf_classifier.predict(X_test)
-
-#--Get the model accuracy
+# Test model
 accuracy = rf_classifier.score(X_test, y_test)
-
 print(f"Model accuracy: {accuracy}")
 
-# test the model on a single observation
 test_data = X_test.iloc[[0]]
-
-# save test data to a txt file
-test_data.to_csv('data/test_data.txt', sep = ',', index = False)
-# print the actual label
+test_data.to_csv('data/test_data.txt', sep=',', index=False)
 print(f"Actual label: {y_test[0]}")
-# print the predicted label
 print(f"Predicted label: {rf_classifier.predict(test_data)[0]}")
 
-# #--Save the trained model to a file
-# model_filename = './src/models/trained_model.joblib'
-# joblib.dump(rf_classifier, model_filename)
-# print("Model trained and saved successfully.")
+# Save model
+model_ref = bentoml.sklearn.save_model("accidents_rf", rf_classifier)
+print(f"Model saved as: {model_ref}")
